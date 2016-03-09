446f66e6fb0e7ecb1c2d209f2affc260fda24bb5
jeanlouisgicquel <jlgicquel22@gmail.com>
2016-03-09 09:43:10 +0100

    json test

--- js/map.js
+++ js/map.js
@@ -1,17 +1,41 @@
-var _map = L.map('mapid').setView([51.505, -0.09], 13);
+$(function() {
 
-L.tileLayer('http://{s}.tile.openstreetmap.se/hydda/base/{z}/{x}/{y}.png', {
-	attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(_map);
+	var _map = L.map('mapid').setView([48.1, -4.4833], 10);
+
+	L.tileLayer('http://{s}.tile.openstreetmap.se/hydda/base/{z}/{x}/{y}.png', {
+		attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	}).addTo(_map);
+
+	var SizeOfIcon = 60;
+	var beaufort = [
+		L.icon({ iconUrl: 'src/img/beaufort/beaufort0.png', iconSize: [SizeOfIcon, SizeOfIcon], iconAnchor: [SizeOfIcon/2, SizeOfIcon]}),
+		L.icon({ iconUrl: 'src/img/beaufort/beaufort1.png', iconSize: [SizeOfIcon, SizeOfIcon], iconAnchor: [SizeOfIcon/2, SizeOfIcon]}),
+		L.icon({ iconUrl: 'src/img/beaufort/beaufort2.png', iconSize: [SizeOfIcon, SizeOfIcon], iconAnchor: [SizeOfIcon/2, SizeOfIcon]}),
+		L.icon({ iconUrl: 'src/img/beaufort/beaufort3.png', iconSize: [SizeOfIcon, SizeOfIcon], iconAnchor: [SizeOfIcon/2, SizeOfIcon]}),
+		L.icon({ iconUrl: 'src/img/beaufort/beaufort4.png', iconSize: [SizeOfIcon, SizeOfIcon], iconAnchor: [SizeOfIcon/2, SizeOfIcon]}),
+		L.icon({ iconUrl: 'src/img/beaufort/beaufort5.png', iconSize: [SizeOfIcon, SizeOfIcon], iconAnchor: [SizeOfIcon/2, SizeOfIcon]}),
+		L.icon({ iconUrl: 'src/img/beaufort/beaufort6.png', iconSize: [SizeOfIcon, SizeOfIcon], iconAnchor: [SizeOfIcon/2, SizeOfIcon]}),
+		L.icon({ iconUrl: 'src/img/beaufort/beaufort7.png', iconSize: [SizeOfIcon, SizeOfIcon], iconAnchor: [SizeOfIcon/2, SizeOfIcon]}),
+		L.icon({ iconUrl: 'src/img/beaufort/beaufort8.png', iconSize: [SizeOfIcon, SizeOfIcon], iconAnchor: [SizeOfIcon/2, SizeOfIcon]}),
+		L.icon({ iconUrl: 'src/img/beaufort/beaufort9.png', iconSize: [SizeOfIcon, SizeOfIcon], iconAnchor: [SizeOfIcon/2, SizeOfIcon]}),
+		L.icon({ iconUrl: 'src/img/beaufort/beaufort10.png', iconSize: [SizeOfIcon, SizeOfIcon], iconAnchor: [SizeOfIcon/2, SizeOfIcon]}),
+		L.icon({ iconUrl: 'src/img/beaufort/beaufort11.png', iconSize: [SizeOfIcon, SizeOfIcon], iconAnchor: [SizeOfIcon/2, SizeOfIcon]}),
+		L.icon({ iconUrl: 'src/img/beaufort/beaufort12.png', iconSize: [SizeOfIcon, SizeOfIcon], iconAnchor: [SizeOfIcon/2, SizeOfIcon]})
+	];
 
 
-var b5 = L.icon({
-	iconUrl: 'src/img/beaufort/beaufort5.png',
-	iconSize:     [100, 100],
-	iconAnchor:   [50, 50]
+	$.getJSON('js/data.json', function (data){
+		$.each(data, function(index, d){
+			console.log(d);
+			L.marker([d.lat, d.long], {icon: beaufort[d.speed], iconAngle: d.direction}).addTo(_map).bindPopup("Latitude : '+ d.lat + '<br>Longitude : '+ d.long + '<br>Vitesse : '+ d.speed + '<br>Direction : '+ d.direction + '°");			
+		});
+	});
 });
 
-L.marker([51.5, -0.09], {icon: b5, iconAngle: -20}).addTo(_map).bindPopup("Longitude : 45<br>Latitude : 23<br>Noeud : 5");
+//L.marker([48.0, -4.2], {icon: b5, iconAngle: 0}).addTo(_map).bindPopup("Longitude : 48.0<br>Latitude : -4.0<br>Vitesse : 5<br>Direction : 0°");
+//L.marker([48.0, -4.0], {icon: b6, iconAngle: 90}).addTo(_map).bindPopup("Longitude : 48.2<br>Latitude : -4.0<br>Vitesse : 5<br>Direction : 0°");
+//L.marker([48.2, -4.2], {icon: b7, iconAngle: 0}).addTo(_map).bindPopup("Longitude : 48.0<br>Latitude : -4.2<br>Vitesse : 5<br>Direction : 0°");
+//L.marker([48.2, -4.0], {icon: b8, iconAngle: 0}).addTo(_map).bindPopup("Longitude : 48.2<br>Latitude : -4.2<br>Vitesse : 5<br>Direction : 0°");
 
 $("#reduce" ).click(function() {
 	if($("#sidebar").css("height") == "0px") {
@@ -20,4 +44,3 @@ $("#reduce" ).click(function() {
 	} else
 		$("#sidebar").css("height", "0px");
 });
-
