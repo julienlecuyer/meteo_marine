c0e84f95aaea8c071301cd1ba15dbb9bed0d3d47
julienlecuyer <lecuyer.ju@gmail.com>
2016-03-11 15:16:42 +0100

    dele

--- parser_grib2/parser.php
+++ parser_grib2/parser.php
@@ -1,9 +1,9 @@
 <?php
-	echo "==> Extraction donnees vents\n";
-	exec("wgrib2\wgrib2 ".$argv[1]." -wind_speed data/out -wind_dir data/out");
-	echo "==> Ecriture donnees WIND\n";
+	//echo "==> Extraction donnees vents\n";
+	exec("wgrib2\wgrib2 ../../gfs.t06z.pgrb2full.0p50.f087 -wind_speed data/out -wind_dir data/out");
+	//echo "==> Ecriture donnees WIND\n";
 	exec('wgrib2\wgrib2 data/out -s | findstr /R /C":WIND:10[ ]*m[ ]above[ ]ground" | wgrib2\wgrib2 -i data/out -spread data/in.txt');
-	echo "==> Ecriture donnees WDIR\n";
+	//echo "==> Ecriture donnees WDIR\n";
 	exec('wgrib2\wgrib2 data/out -s | findstr /R /C":WDIR:10[ ]*m[ ]above[ ]ground" | wgrib2\wgrib2 -i data/out -append -spread data/in.txt');
 
 
@@ -19,48 +19,54 @@
 				's'	=> 0,
 				'd'	=> null
 			);
+	$latNW  = $_GET['latNW'];
+	$lngNW  = $_GET['lngNW'];
+	$latSE  = $_GET['latSE'];
+	$lngSE  = $_GET['lngSE'];
 
 	$i = 0;
 	if ($handle) {
-		echo "==> Lecture fichier :\n";
+		//echo "==> Lecture fichier :\n";
 		while (!feof($handle)) {
 			$buffer = fgets($handle);
 			if(strcmp(explode(" ", $buffer)[0],"lon,lat,WIND") == 0) {
-				echo "Mode SPEED\n";
+				//echo "Mode SPEED\n";
 				$mode = 0;
 				$buffer = fgets($handle);
 			} else if(strcmp(explode(" ", $buffer)[0],"lon,lat,WDIR") == 0) {
-				echo "Mode DIR\n";
+				//echo "Mode DIR\n";
 				$mode = 1;
 				$date = explode("d=", $buffer)[1];
 				$buffer = fgets($handle);
 			}
-			if($mode == 0) {
-				$val = explode(",", $buffer);
-				
-				$data["y"] = number_format((float)$val[1], 1, '.', '');
-				$data["x"]	= number_format((float)$val[0], 1, '.', '');
-				$data["s"]	= $val[2];
-				array_push($array, $data);
-			} elseif($mode == 1) {
-				$val = explode(",", $buffer);
-				//var_dump($val);
-				if(strcmp($val[0], "") != 0) {
-					$array[$i]["d"] = $val[2];
-					$i++;
+			$val = explode(",", $buffer);
+			if($val[1] < $latNW && $val[0] > $lngNW && $val[1] > $latSE && $val[0] < $lngSE) {
+				if($mode == 0) {
+					$data["y"] = number_format((float)$val[1], 1, '.', '');
+					$data["x"]	= number_format((float)$val[0], 1, '.', '');
+					$data["s"]	= $val[2];
+					array_push($array, $data);
+				} elseif($mode == 1) {
+					$val = explode(",", $buffer);
+					//var_dump($val);
+					if(strcmp($val[0], "") != 0) {
+						$array[$i]["d"] = $val[2];
+						$i++;
+					}
 				}
 			}
 		}
-		echo "==> Fermeture fichier\n";
+		//echo "==> Fermeture fichier\n";
 		fclose($handle);
 	}
-	echo "==> JSON ENCODE";
+	//echo "==> JSON ENCODE";
 	$json = json_encode($array);
-	echo "<Correction des caractères speciaux>\n";
+	//echo "<Correction des caractères speciaux>\n";
 	$json = str_replace('\r\n','', $json);
 	$json = str_replace('\n','', $json);
-	echo "==> Ecriture fichier json";
-	$jsonfic = fopen("json/".explode(" ", $date)[0].".json", 'w+');
-	fputs($jsonfic, $json);
-	echo "FIN DU PROGRAMME";
+	//echo "==> Ecriture fichier json";
+	//$jsonfic = fopen("json/".explode(" ", $date)[0].".json", 'w+');
+	//fputs($jsonfic, $json);
+	echo $json;
+	//echo "FIN DU PROGRAMME";
 ?>
